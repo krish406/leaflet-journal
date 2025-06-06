43f03f8f6ec6fd53ac547c736a95f84f104ca8b8
krish406 <155498135+krish406@users.noreply.github.com>
2025-06-05 20:57:22 -0400

    added the ability to place and remove markers

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+new L.Control.Geocoder().addTo(map);
+
+map.on("click", function(e){
+    var marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map).on('click', e => e.target.remove());
+    marker.className = "hello";
+    console.log(marker);
+})
