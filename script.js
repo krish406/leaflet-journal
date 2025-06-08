e16acb4ee53ddc2c72cbe3aa16929f38057625e2
krish406 <155498135+krish406@users.noreply.github.com>
2025-06-08 14:45:49 -0400

    added bounding boxes to maps

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //add geocoder
-new L.Control.Geocoder().addTo(map);
+let geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false,
+    collapsed: false
+  })
+  .on('markgeocode', function(e) {
+    var bbox = e.geocode.bbox;
+    var poly = L.polygon([
+      bbox.getSouthEast(),
+      bbox.getNorthEast(),
+      bbox.getNorthWest(),
+      bbox.getSouthWest()
+    ]);
+    map.fitBounds(poly.getBounds());
+  })
+  .addTo(map);
 
 function marker_add_listener(element){
     return element.on('click', (evt) => {
         let coordinates_rem = [evt.latlng.lat, evt.latlng.lng];
-        
+        const allDescriptors = document.querySelectorAll(".location-element");
+
         //remove from local storage
         let all_coords = localStorage.getItem('savedMarkers');
         all_coords = JSON.parse(all_coords);
@@ -23,11 +38,19 @@ function marker_add_listener(element){
         
         if (index_to_remove > -1) { 
             all_coords.splice(index_to_remove, 1); 
+            allDescriptors[index_to_remove].remove();
         }
 
         localStorage.setItem('savedMarkers', JSON.stringify(all_coords));
         evt.target.remove();
     });
+}
+
+function createMarkerDescriptor(coordinate){
+    let marker_name = document.createElement('p');
+    marker_name.className = "location-element";
+    marker_name.textContent = `${coordinate[0]}, ${coordinate[1]}`;
+    sidebar.appendChild(marker_name);
 }
 
 //add and delete markers on map click
@@ -39,6 +62,8 @@ map.on("click", function(e){
     //when a marker is clicked it will be removed from local storage and the map
     marker_add_listener(marker);
     all_markers.push(coordinates);
+    createMarkerDescriptor(coordinates);
+
     console.log(all_markers);
     localStorage.setItem('savedMarkers', JSON.stringify(all_markers));
     console.log(localStorage);
@@ -56,4 +81,5 @@ savedMarkers = JSON.parse(savedMarkers);
 savedMarkers.forEach(function(coord) {
     let marker = L.marker(coord).addTo(map);
     marker_add_listener(marker);
+    createMarkerDescriptor(coord);
 });
