ae71ae5701c50a16661bca2701060eeb804acab6
krish406 <155498135+krish406@users.noreply.github.com>
2025-06-07 21:52:26 -0400

    improved state management by removing global array now everything is handled in local storage, but empty json is a pain

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function marker_add_listener(element){
     return element.on('click', (evt) => {
         let coordinates_rem = [evt.latlng.lat, evt.latlng.lng];
         
+        //remove from local storage
         let all_coords = localStorage.getItem('savedMarkers');
         all_coords = JSON.parse(all_coords);
 
@@ -22,7 +23,6 @@ function marker_add_listener(element){
         
         if (index_to_remove > -1) { 
             all_coords.splice(index_to_remove, 1); 
-            markers.splice(index_to_remove, 1);
         }
 
         localStorage.setItem('savedMarkers', JSON.stringify(all_coords));
@@ -30,35 +30,30 @@ function marker_add_listener(element){
     });
 }
 
-//store and rerender markers
-let markers = [];
-let savedMarkers = localStorage.getItem('savedMarkers');
-if (savedMarkers) {
-    let coordinates = JSON.parse(savedMarkers);
-    coordinates.forEach(function(coord) {
-        let marker = L.marker(coord).addTo(map);
-        markers.push(coord);
-        marker_add_listener(marker);
-    });
-}
-
 //add and delete markers on map click
 map.on("click", function(e){
     let coordinates = [e.latlng.lat, e.latlng.lng];
-    let marker = new L.marker(coordinates).addTo(map)
-    // let location = document.createElement("p");
-    // location.className = "coord";
-    // location.textContent = `${coordinates[0]}, ${coordinates[1]}`;
-    // sidebar.appendChild(location);
-    // location.style.color = "white"
+    let marker = new L.marker(coordinates).addTo(map);
+    let all_markers = JSON.parse(localStorage.getItem('savedMarkers'));
 
     //when a marker is clicked it will be removed from local storage and the map
     marker_add_listener(marker);
-
-    markers.push(coordinates);
-    console.log(markers);
-
-    localStorage.setItem('savedMarkers', JSON.stringify(markers));
+    all_markers.push(coordinates);
+    console.log(all_markers);
+    localStorage.setItem('savedMarkers', JSON.stringify(all_markers));
     console.log(localStorage);
 });
 
+//store and rerender markers
+let savedMarkers = localStorage.getItem('savedMarkers');
+if(!savedMarkers){
+    console.log("hello");
+    localStorage.setItem('savedMarkers', '[]');
+    console.log(localStorage);
+}
+
+savedMarkers = JSON.parse(savedMarkers);
+savedMarkers.forEach(function(coord) {
+    let marker = L.marker(coord).addTo(map);
+    marker_add_listener(marker);
+});
